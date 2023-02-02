0b2866457cf84339b9f9cddc0d0cd40ff7012b06
Alvin <alvinmartindwrk@gmail.com>
2023-02-02 19:54:43 +0700

    add progress bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
+import NextNProgress from "nextjs-progressbar";
 
 import { api } from "../utils/api";
 
@@ -13,6 +14,7 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
+      <NextNProgress />
       <MainLayout>
         <Component {...pageProps} />
       </MainLayout>
