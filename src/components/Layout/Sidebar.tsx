c5e7b0fdcd3ae5d9466d6c92cdef4f81c5e0e4ae
Alvin <alvinmartindwrk@gmail.com>
2023-02-02 20:06:42 +0700

    close drawer on menu click for mobile size

--- src/components/Layout/Sidebar.tsx
+++ src/components/Layout/Sidebar.tsx
@@ -2,10 +2,13 @@ import clsx from "clsx";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import type { ReactNode } from "react";
+import { useRef } from "react";
 import Navbar from "./Navbar";
 
 const Sidebar = ({ content }: { content: ReactNode }) => {
   const router = useRouter();
+
+  const toggleRef = useRef<HTMLInputElement>(null);
 
   const drawerId = "my-drawer";
 
@@ -24,9 +27,20 @@ const Sidebar = ({ content }: { content: ReactNode }) => {
     },
   ];
 
+  function handleClick() {
+    if (toggleRef.current) {
+      toggleRef.current.checked = false;
+    }
+  }
+
   return (
     <div className="drawer-mobile drawer">
-      <input id={drawerId} type="checkbox" className="drawer-toggle" />
+      <input
+        id={drawerId}
+        type="checkbox"
+        className="drawer-toggle"
+        ref={toggleRef}
+      />
       <div className="drawer-content flex flex-col">
         <Navbar drawerId={drawerId} />
         <div className="p-2">{content}</div>
@@ -34,13 +48,18 @@ const Sidebar = ({ content }: { content: ReactNode }) => {
       <div className="drawer-side shadow">
         <label htmlFor={drawerId} className="drawer-overlay"></label>
         <ul className="menu w-80 gap-2 bg-base-100 p-4">
-          <Link href="/dashboard" className="btn-ghost btn text-xl normal-case">
+          <Link
+            href="/dashboard"
+            onClick={handleClick}
+            className="btn-ghost btn text-xl normal-case"
+          >
             Admin Dashboard
           </Link>
           {menus.map((menu, index) => (
             <li key={index}>
               <Link
                 href={menu.to}
+                onClick={handleClick}
                 className={clsx(
                   "btn-outline btn font-bold",
                   router.pathname.startsWith(menu.to) && "btn-active"
