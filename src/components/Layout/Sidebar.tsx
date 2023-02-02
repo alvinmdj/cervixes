e678049e9c6a98252ac9d06d267de189907f0e85
Alvin <alvinmartindwrk@gmail.com>
2023-02-02 15:17:48 +0700

    refactor sidebar menu mapping

--- src/components/Layout/Sidebar.tsx
+++ src/components/Layout/Sidebar.tsx
@@ -4,25 +4,38 @@ import Navbar from "./Navbar";
 
 const Sidebar = ({ content }: { content: ReactNode }) => {
   const drawerId = "my-drawer";
+
+  const menus = [
+    {
+      name: "Manage Diseases",
+      to: "/dashboard/diseases",
+    },
+    {
+      name: "Manage Symptoms",
+      to: "/dashboard/symptoms",
+    },
+  ];
+
   return (
     <div className="drawer-mobile drawer">
       <input id={drawerId} type="checkbox" className="drawer-toggle" />
       <div className="drawer-content flex flex-col">
         <Navbar drawerId={drawerId} />
-        {content}
+        <div className="p-2">{content}</div>
       </div>
-      <div className="drawer-side">
+      <div className="drawer-side shadow">
         <label htmlFor={drawerId} className="drawer-overlay"></label>
-        <ul className="menu w-80 bg-base-100 p-4">
-          <Link href="/dashboard" className="btn mb-2">
+        <ul className="menu w-80 gap-2 bg-base-100 p-4">
+          <Link href="/dashboard" className="btn">
             Admin Dashboard
           </Link>
-          <li>
-            <a>Sidebar Item 1</a>
-          </li>
-          <li>
-            <a>Sidebar Item 2</a>
-          </li>
+          {menus.map((menu, index) => (
+            <li key={index}>
+              <Link href={menu.to} className="btn-outline btn font-bold">
+                {menu.name}
+              </Link>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
