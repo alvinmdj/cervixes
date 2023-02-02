a538232d74c747acc2b7180663095567268ed2a8
Alvin <alvinmartindwrk@gmail.com>
2023-02-02 15:18:42 +0700

    initial protected admin only page

---
+++ src/pages/dashboard/diseases.tsx
@@ -0,0 +1,25 @@
+import type { GetServerSideProps } from "next";
+import { getServerAuthSession } from "../../server/auth";
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getServerAuthSession(context);
+
+  if (!session || session.user.role === "USER") {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+};
+
+const Diseases = () => {
+  return <div>Diseases</div>;
+};
+
+export default Diseases;
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,6 +1,32 @@
+import type { GetServerSideProps } from "next";
+import { getServerAuthSession } from "../../server/auth";
+import { api } from "../../utils/api";
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getServerAuthSession(context);
+
+  if (!session || session.user.role === "USER") {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+};
+
 const Dashboard = () => {
+  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+
   return (
-    <div className="p-2">
+    <div>
+      <p className="text-2xl text-black">
+        {hello.data ? hello.data.greeting : "Loading tRPC query..."}
+      </p>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
         laudantium facere maxime consequuntur est voluptatem qui unde, rerum
