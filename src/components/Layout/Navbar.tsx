d3863d0d902d33af0bedb62200954f21a3f00ff5
Alvin <alvinmartindwrk@gmail.com>
2023-02-03 20:54:22 +0700

    implement auth logic for component at navbar

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -1,7 +1,23 @@
+import { signIn, signOut, useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
+import { toast } from "react-hot-toast";
 
 const Navbar = ({ drawerId }: { drawerId?: string }) => {
+  const { data: session, status } = useSession();
+
+  const handleSignIn = () => {
+    toast.loading("Redirecting...");
+    void signIn("google", {
+      callbackUrl: window.location.href, // current url
+    });
+  };
+
+  const handleSignOut = () => {
+    toast.loading("Signing out...");
+    void signOut();
+  };
+
   return (
     <div className="navbar bg-base-100 shadow">
       {drawerId && (
@@ -29,35 +45,36 @@ const Navbar = ({ drawerId }: { drawerId?: string }) => {
         </Link>
       </div>
       <div className="flex-none gap-2">
-        <div className="dropdown-end dropdown">
-          <label tabIndex={0} className="btn-ghost btn-circle avatar btn">
-            <div className="w-10 rounded-full border">
-              <Image
-                src="/profile.jpg"
-                alt="Profile Image"
-                width={40}
-                height={40}
-              />
-            </div>
-          </label>
-          <ul
-            tabIndex={0}
-            className="dropdown-content menu rounded-box menu-compact mt-3 w-52 bg-base-100 p-2 shadow"
-          >
-            <li>
-              <a className="justify-between">
-                Profile
-                <span className="badge">New</span>
-              </a>
-            </li>
-            <li>
-              <a>Settings</a>
-            </li>
-            <li>
-              <a>Logout</a>
-            </li>
-          </ul>
-        </div>
+        {status === "loading" && (
+          <div className="h-8 w-[75px] animate-pulse rounded-md bg-gray-200" />
+        )}
+        {status === "unauthenticated" && (
+          <button className="btn-sm btn" onClick={handleSignIn}>
+            Sign in
+          </button>
+        )}
+        {status === "authenticated" && (
+          <div className="dropdown-end dropdown">
+            <label tabIndex={0} className="btn-ghost btn-circle avatar btn">
+              <div className="w-10 rounded-full border">
+                <Image
+                  src={session.user.image || "/profile.jpg"}
+                  alt="Profile Image"
+                  width={40}
+                  height={40}
+                />
+              </div>
+            </label>
+            <ul
+              tabIndex={0}
+              className="dropdown-content menu rounded-box menu-compact mt-3 w-52 bg-base-100 p-2 shadow"
+            >
+              <li>
+                <span onClick={handleSignOut}>Sign out</span>
+              </li>
+            </ul>
+          </div>
+        )}
       </div>
     </div>
   );
