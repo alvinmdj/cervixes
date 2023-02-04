e8250072b9398fa2d2eed35f5bdfb08ed3138eab
Alvin <alvinmartindwrk@gmail.com>
2023-02-04 20:33:38 +0700

    remove unused commented code

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -89,14 +89,6 @@ const Navbar = ({ drawerId }: { drawerId?: string }) => {
                 </div>
               </div>
             </div>
-            {/* <ul
-              tabIndex={0}
-              className="dropdown-content menu rounded-box menu-compact mt-3 w-52 bg-base-100 p-2 shadow"
-            >
-              <li>
-                <span onClick={handleSignOut}>Sign out</span>
-              </li>
-            </ul> */}
           </div>
         )}
       </div>
