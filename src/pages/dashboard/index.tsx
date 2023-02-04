6bc1b5abecbeb592b91f4ca7b698d78fb449c0fb
Alvin <alvinmartindwrk@gmail.com>
2023-02-04 22:39:46 +0700

    remove example router

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,14 +1,8 @@
 import AdminCheck from "../../components/AdminCheck";
-import { api } from "../../utils/api";
 
 const Dashboard = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
-
   return (
     <AdminCheck>
-      <p className="text-2xl text-black">
-        {hello.data ? hello.data.greeting : "Loading tRPC query..."}
-      </p>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
         laudantium facere maxime consequuntur est voluptatem qui unde, rerum
